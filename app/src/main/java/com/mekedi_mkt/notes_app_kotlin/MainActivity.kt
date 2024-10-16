c8970185e47b61b8a5a06ba5dfcdb222eaa00a82
mekedi-mkt <mekedi.mkt@gmail.com>
2024-10-16 16:29:07 +0300

    VIewModel & Adapter

--- app/src/main/java/com/mekedi_mkt/notes_app_kotlin/MainActivity.kt
+++ app/src/main/java/com/mekedi_mkt/notes_app_kotlin/MainActivity.kt
@@ -5,16 +5,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.mekedi_mkt.notes_app_kotlin.database.NoteDatabase
+import com.mekedi_mkt.notes_app_kotlin.model.Note
+import com.mekedi_mkt.notes_app_kotlin.repository.NoteRepository
+import com.mekedi_mkt.notes_app_kotlin.viewmodel.NoteViewModel
+import com.mekedi_mkt.notes_app_kotlin.viewmodel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var noteViewModel: NoteViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+
+        setUpViewModel()
+    }
+
+    private fun setUpViewModel() {
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
     }
 }
---
+++ app/src/main/java/com/mekedi_mkt/notes_app_kotlin/adapter/NoteAdapter.kt
@@ -0,0 +1,53 @@
+package com.mekedi_mkt.notes_app_kotlin.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.mekedi_mkt.notes_app_kotlin.databinding.NoteLayoutBinding
+import com.mekedi_mkt.notes_app_kotlin.fragments.HomeFragmentDirections
+import com.mekedi_mkt.notes_app_kotlin.model.Note
+
+class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+    class NoteViewHolder(val itemBinding: NoteLayoutBinding) :
+        RecyclerView.ViewHolder(itemBinding.root)
+
+    private val differCallback = object : DiffUtil.ItemCallback<Note>() {
+        override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem.id == newItem.id &&
+                    oldItem.title == newItem.title &&
+                    oldItem.description == newItem.description
+        }
+
+        override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem == newItem
+        }
+    }
+    val differ = AsyncListDiffer(this, differCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteAdapter.NoteViewHolder {
+        return NoteViewHolder(
+            NoteLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: NoteAdapter.NoteViewHolder, position: Int) {
+        val currentNote= differ.currentList[position]
+
+        holder.itemBinding.noteTitle.text = currentNote.title
+        holder.itemBinding.noteDesc.text = currentNote.description
+
+        holder.itemView.setOnClickListener {
+            val direction = HomeFragmentDirections.actionHomeFragment3ToEditNoteFragment2(currentNote)
+            it.findNavController().navigate(direction)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+}
